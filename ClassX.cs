ac24412a996d8ffc155390551e17b65d62e51a05
realniceping <86911372+realniceping@users.noreply.github.com>
2022-04-22 23:26:25 +0500

    C# delegate and events my explanation

---
+++ ClassX.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace delegateAndEvent{
+
+    class classX{
+
+        public void ReactoinOnFire(){
+            System.Console.WriteLine("Class X seen that guns are fired");
+        }
+
+
+
+    } 
+
+}
---
+++ ClassY.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace delegateAndEvent{
+
+    class classY{
+
+        public void ReactoinOnFire(){
+            System.Console.WriteLine("Class Y also seen that gut are fired");
+        }
+        
+
+
+    } 
+
+}
